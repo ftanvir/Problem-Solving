0048ee5cc8fb3f1d53fd12076facb3994b08f314
ftanvir <ft.tanvir551@gmail.com>
2021-10-21 00:30:21 +0600

    solved

---
+++ A_Remove_a_Progression.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() 
+{
+    ll t;
+    cin>>t;
+    while(t--) {
+        ll n, x;
+        cin>>n>>x;
+        cout<<2*x<<endl;
+    }
+
+    return 0;   
+}
