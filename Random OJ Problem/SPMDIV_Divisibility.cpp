b21e91b26aa0b487f518ed29f26f06e0efb298a4
ftanvir <ft.tanvir551@gmail.com>
2021-11-07 20:31:15 +0600

    solved

---
+++ Random OJ Problem/SPMDIV_Divisibility.cpp
@@ -0,0 +1,24 @@
+/*
+Sphere Online Judge Problem
+Problem link: https://www.spoj.com/problems/SMPDIV/
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) {
+        int n, x, y;
+        cin>>n>>x>>y;
+        for(int i=1; i<n; i++) {
+            if( i%x==0 && i%y!=0) {
+                cout<<i<<" ";
+            }
+        } cout<<endl;
+    }
+    return 0;
+}
