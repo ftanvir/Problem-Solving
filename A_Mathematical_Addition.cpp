272cb1bbb079d6707127a41074ae0c230fc406d0
ftanvir <ft.tanvir551@gmail.com>
2021-11-14 16:42:54 +0600

    solved

---
+++ A_Mathematical_Addition.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--) {
+        ll u, v;
+        cin>>u>>v;
+        ll x = - (u*u);
+        ll y = (v*v);
+
+        cout<<x<<" "<<y<<endl;
+    }
+
+    return 0;
+}
