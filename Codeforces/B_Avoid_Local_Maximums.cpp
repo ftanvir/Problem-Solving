e61c3df795de2d0ec1cf899decd2ba4839e81512
ftanvir <ft.tanvir551@gmail.com>
2022-03-27 03:21:58 +0600

    solved

---
+++ Codeforces/B_Avoid_Local_Maximums.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define fast      ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define pi          acos(-1.0)
+#define nl          '\n'
+ 
+/**-------Shortend Library-------*/
+
+#define vi vector<int>
+#define mii map<int, int>
+
+#define ff first
+#define ss second
+#define all(a)         a.begin(),a.end()
+#define Sort(x)        sort(x.begin(),x.end())
+#define SortR(x)       sort(x.begin(),x.end(),greater<int>())
+#define Reverse(x)     reverse(x.begin(),x.end())
+
+ 
+
+#define SortA(ar,s)    sort(ar,ar+s)
+#define SortD(ar,s)    sort(ar,ar+s,greater<int>())
+
+#define gcd(a,b)       __gcd(a,b)
+#define lcm(a,b)       (a*(b/gcd(a,b)))
+#define sq(x)          (x)*(x)
+#define mid(l,r)       (l+(r-l)/2)
+ 
+
+#define leftShift(p)         (p<<1)
+#define rightShift(p)        (p>>1)
+ 
+#define min3(a,b,c)       min(a,min(b,c))
+#define min4(a,b,c,d)     min(a,min(b,min(c,d)))
+#define max3(a,b,c)       max(a,max(b,c))
+#define max4(a,b,c,d)     max(a,max(b,max(c,d)))
+#define pb(x)             push_back(x)
+#define mod               1e9+7
+#define precision(x)      cout<<fixed<<setprecision(x)
+#define yes               cout<<"YES"<<nl
+#define no                cout<<"NO"<<nl
+ 
+
+ 
+/**----data type----*/
+#define ll long long int
+#define llu unsigned long long int
+
+/**----Functions to use----**/
+
+ll powerLog(ll base, ll power) 
+{
+    ll res = 1;
+    while(power) 
+    {
+        if(power%2) 
+        {
+            res *= base;
+            power--;
+        } else 
+        {
+            base *=base;
+            power/=2;
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    fast;
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        ll n;
+        cin>>n;
+        vi arr;
+        for(int i=0; i<n; i++) {
+            int num;
+            cin>>num;
+            arr.pb(num);
+        }
+        int cnt=0;
+        for(int i=1; i<n-1; i++) {
+            if(arr[i]>arr[i-1] && arr[i]>arr[i+1]) {
+                cnt++;
+                if(i+2<n) {
+                    arr[i+1] = max(arr[i], arr[i+2]);
+                } else {
+                    arr[i+1] = arr[i];
+                }
+            }
+        }
+        cout<<cnt<<nl;
+        for(int i=0; i<n; i++) {
+            cout<<arr[i]<<" ";
+        }cout<<nl;
+    }
+    
+    return 0;
+}
